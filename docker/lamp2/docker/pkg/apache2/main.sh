a80412cf0ac1e4490016c7ca052dc2bfc367b742
VladVons <vladvons@gmail.com>
2023-03-17 19:19:16 +0200

    MyCommit

--- docker/lamp2/docker/pkg/apache2/main.sh
+++ docker/lamp2/docker/pkg/apache2/main.sh
@@ -1,8 +1,13 @@
 PostInstall()
 {
     a2enmod rewrite
-    a2enmod actions fcgid alias proxy_fcgi
+    a2enmod actions fcgid alias proxy_fcgi ssl
 
     usermod -G staff www-data
     chmod 777 /var/www
+
+    # ssl for https
+    mkdir /etc/apache2/certificate
+    cd /etc/apache2/certificate
+    openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out apache-certificate.crt -keyout apache.key
 }
